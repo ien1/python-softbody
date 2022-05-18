85c39d413d418b3466a64bfdba88d77aa2e408b2
parasite <98902610+ien1@users.noreply.github.com>
2022-05-18 17:51:17 +0200

    Add files via upload

---
+++ example.py
@@ -0,0 +1,137 @@
+
+from math import sqrt
+import pygame
+import pymunk
+import pymunk.pygame_util as pygu
+from softbody import SoftBody
+
+
+def create_ball(space, radius:float, mass:float, position:int):
+    """Creating a ball in pymunk.Space.
+
+    Args:
+        space: pymunk space
+        radius (float): Radius of the ball.
+        mass (float): Mass of the ball.
+        position (int): Where the ball should be placed to.
+
+    Returns:
+        pymunk.Circle
+    """
+    body = pymunk.Body()
+    body.position = position
+    shape = pymunk.Circle(body, radius)
+    shape.mass = mass
+    # shape.color = (255, 0, 0, 100)
+    space.add(body, shape)
+    shape.elasticity = 0.9
+    shape.friction = 1
+    return shape
+
+
+def create_boundaries(space, width, height):
+    """Create boundaries (walls at the top, left, bottom and right).
+
+    Args:
+        space (pymunk.Space): pymunk space
+        width (int): screen width
+        height (int): screen height
+    """
+    rects = [
+        [(width / 2, height - 10), (width, 20)],
+        [(width / 2, 10), (width, 20)],
+        [(10, height / 2), (20, height)],
+        [(width - 10, height / 2), (20, height)]
+    ]
+    for pos, size in rects:
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        body.position = pos
+        shape = pymunk.Poly.create_box(body, size)
+        shape.elasticity = 0.4
+        shape.friction = 0.5
+        space.add(body, shape)
+
+
+def draw(space:pymunk.Space, window, draw_options):
+    """Drawing the pymunk objects on pygame screen.
+
+    Args:
+        space: pymunk space
+        window: pygame screen
+        draw_options: pymunk settings to draw on pygame (pymunk.pygame_util)
+    """
+
+    window.fill("white")
+    space.debug_draw(draw_options)
+    pygame.display.update()
+
+
+if __name__ == "__main__":
+    pygame.init()
+
+    # window settings
+    WIDTH, HEIGHT = 800, 800
+    fps = 144
+
+    # Softbody Properties
+    # springs:
+    STIFFNESS = 1000
+    DAMPING = 1
+    REST_LENGTH = 25
+    # balls:
+    BALL_MASS = 0
+    BALL_RADIUS = 0
+
+    # additional ball properties
+    ADD_BALL_MASS = 1
+    ADD_BALL_RAD = 5
+
+
+    # pygame screen
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    # variable to control window behaviour
+    run = True
+    
+    # pygame clock
+    clock = pygame.time.Clock()
+    dt = 1 / fps
+    
+    # create pymunk space and set its gravity
+    space = pymunk.Space()
+    space.gravity = (0, 981)
+
+    # create boundaries (walls at the top, left, bottom and right)
+    create_boundaries(space, WIDTH, HEIGHT)
+    
+    # initialize module to draw on pygame screen
+    draw_options = pygu.DrawOptions(screen)
+    
+    # saving additional balls
+    additional_balls = []
+
+    # creating softbody
+    softbody = SoftBody(space, (100, 100), 5, 5, REST_LENGTH, STIFFNESS, DAMPING)
+
+    # pygame main loop
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # adding ball to the position of the cursor when clicking on window
+                pos = pygame.mouse.get_pos()
+                additional_balls.append(create_ball(space, ADD_BALL_RAD, ADD_BALL_MASS, pos))
+                if len(additional_balls) == 10:
+                    del additional_balls[0]
+        
+        # drawing onto screen
+        draw(space, screen, draw_options)
+
+        # go to next step of animation
+        space.step(dt)
+        clock.tick(fps)
+    
+    pygame.quit()
---
+++ softbody.py
@@ -0,0 +1,96 @@
+from math import sqrt
+import pygame
+import pymunk
+import pymunk.pygame_util as pygu
+
+class SoftBody():
+    def __init__(self, space, position, x, y, rest_length, stiffness, damping, ball_mass=0.1, ball_radius=0.1):
+        """SoftBody Class - soft body simulation using mass points and damped springs.
+
+        Args:
+            space (_type_): pymunk space
+            position (_type_): Position of the soft body (anchor: north west)
+            x (int): number of mass points in a row
+            y (int): number of mass points in a column
+            rest_length (float): default distance between to points (horizontally and vertically)
+            stiffness (float): stiffness of the springs
+            damping (float): damping factor
+            ball_mass (float, optional): mass of mass points. Defaults to 0.1.
+            ball_radius (float, optional): radius of mass points. Defaults to 0.1.
+        """
+        self.rest_length = rest_length
+        self.stiffness = stiffness
+        self.damping = damping
+        self.ball_mass = ball_mass
+        self.ball_radius = ball_radius
+        self.__space = space
+        self.__balls = []
+        self.__springs = []
+        self.position = position
+        self.create(x, y)
+    
+    def create_ball(self, position):
+        """Creating a ball and adding it to the pymunk space.
+
+        Args:
+            position (list): position of the ball
+
+        Returns:
+            pymunk.Circle
+        """
+        body = pymunk.Body()
+        body.position = position
+        shape = pymunk.Circle(body, self.ball_radius)
+        shape.mass = self.ball_mass
+        # shape.color = (255, 0, 0, 100)
+        self.__space.add(body, shape)
+        shape.elasticity = 0.9
+        shape.friction = 1
+        return shape
+    
+    def create_spring(self, a, b, rest_length=None):
+        """Creating a damped spring that connects two mass points (a and b)
+
+        Args:
+            a (pymunk.Body): mass point a
+            b (pymunk.Body): mass point b
+            rest_length (float, optional): distance between the two points (since it can vary for diagonal springs). Defaults to None.
+
+        Returns:
+            pymunk.constraints.DampedSpring
+        """
+        if not rest_length:
+            rest_length = self.rest_length
+        shape = pymunk.constraints.DampedSpring(a, b, (0, 0), (0, 0), rest_length, self.stiffness, self.damping)
+        self.__space.add(shape)
+        return shape
+    
+    def create(self, x, y):
+        """Create all balls and connect them with damped springs.
+
+        Args:
+            x (int): number of mass points in a row
+            y (int): number of mass points in a column
+        """
+
+        # Create balls
+        for i in range(x):
+            self.__balls.append([])
+            for j in range(y):
+                self.__balls[i].append(self.create_ball((i * self.rest_length + self.position[0], j * self.rest_length + self.position[1])))
+
+        # horizontal + vertical springs
+        for i in range(x):
+            for j in range(y):
+                if i + 1 < x:
+                    self.__springs.append(self.create_spring(self.__balls[i][j].body, self.__balls[i + 1][j].body))
+                if j + 1 < y:
+                    self.__springs.append(self.create_spring(self.__balls[i][j].body, self.__balls[i][j + 1].body))
+        
+        # diagonal springs
+        for i in range(x):
+            for j in range(y):
+                if i + 1 < x and j + 1 < y:
+                    self.__springs.append(self.create_spring(self.__balls[i][j].body, self.__balls[i + 1][j + 1].body, sqrt(2 * self.rest_length ** 2)))
+                if i - 1 > -1 and j + 1 < y:
+                    self.__springs.append(self.create_spring(self.__balls[i][j].body, self.__balls[i - 1][j + 1].body, sqrt(2 * self.rest_length ** 2)))
